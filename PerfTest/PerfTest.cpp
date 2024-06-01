eb1240cc633c92ccc9f6f2021be63595b18f7ca7
Łukasz Sowa <me@sowa705.dev>
2024-06-02 00:54:15 +0200

    Add project files.

---
+++ PerfTest/PerfTest.cpp
@@ -0,0 +1,255 @@
+// PerfTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <windows.h>
+struct xoshiro256p_state
+{
+    uint64_t s[4];
+};
+
+uint64_t rol64(uint64_t x, int k)
+{
+    return (x << k) | (x >> (64 - k));
+}
+
+uint64_t xoshiro256p(struct xoshiro256p_state* state)
+{
+    uint64_t* s = state->s;
+    uint64_t const result = s[0] + s[3];
+    uint64_t const t = s[1] << 17;
+
+    s[2] ^= s[0];
+    s[3] ^= s[1];
+    s[1] ^= s[2];
+    s[0] ^= s[3];
+
+    s[2] ^= t;
+    s[3] = rol64(s[3], 45);
+
+    return result;
+}
+
+uint64_t lcg(uint64_t state) {
+    return state * 6364136223846793005 + 1;
+}
+
+xoshiro256p_state xoshiro256p_init(uint64_t seed)
+{
+	struct xoshiro256p_state state;
+	state.s[0] = seed;
+	state.s[1] = 0x9E3779B97F4A7C15;
+	state.s[2] = 0x9E3779B97F4A7C15;
+	state.s[3] = 0x9E3779B97F4A7C15;
+
+    for (int i = 0; i < 100; i++)
+    {
+		xoshiro256p(&state);
+	}
+
+	return state;
+}
+
+// perform mem copy test, return speed in gigabytes/second
+uint64_t performCopyTest(size_t count, int threadId, size_t* startval, double* scores)
+{
+    size_t* array1 = new size_t[count];
+
+    // initialize array1
+
+    struct xoshiro256p_state state = xoshiro256p_init(time(NULL) + threadId);
+
+    for (size_t i = 0; i < count; i++)
+    {
+        array1[i] = xoshiro256p(&state);
+    }
+
+    size_t iters = 2.0 / ((double)count / 1024.0 / 1024.0 / 1024.0);
+
+    while (startval[0] != 0x1234)
+    {
+		// wait until start is 0x1234
+	}
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    size_t* src = array1;
+
+    uint64_t sum = 0;
+    for (size_t i = 0; i < iters; i++)
+    {
+        //std::copy(src, src + count, dst);
+
+        for (size_t j = 0; j < count/8; j++)
+        {
+            const size_t a0 = src[j * 8 + 0];
+            const size_t a1 = src[j * 8 + 1];
+            const size_t a2 = src[j * 8 + 2];
+            const size_t a3 = src[j * 8 + 3];
+            const size_t a4 = src[j * 8 + 4];
+            const size_t a5 = src[j * 8 + 5];
+            const size_t a6 = src[j * 8 + 6];
+            const size_t a7 = src[j * 8 + 7];
+
+            sum += a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7;
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> elapsed = end - start;
+
+    double seconds = elapsed.count() / iters;
+    double speed = count * sizeof(size_t) / seconds / 1024 / 1024 / 1024;
+
+    scores[threadId] = speed;
+
+    delete[] array1;
+
+    return sum;
+}
+
+
+
+uint64_t memRandAccess(size_t count, int threadId, size_t* startval, double* scores)
+{
+
+}
+
+std::atomic<size_t> testatomic(0);
+
+void c2ctest(int threadId, int otherThreadId, size_t* startval, double* scores)
+{
+    while (startval[0] != 0x1234)
+    {
+        // wait until start is 0x1234
+    }
+
+    size_t iters = 1500000;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (size_t i = 0; i < iters; i++)
+    {
+        size_t expected = otherThreadId;
+        while (!testatomic.compare_exchange_weak(expected, threadId))
+        {
+			expected = otherThreadId;
+		}
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> elapsed = end - start;
+
+    double seconds = elapsed.count();
+
+    double speed = (iters * 2) / seconds;
+
+    if (scores == nullptr)
+    {
+        return;
+    }
+
+    int hwThreads = std::thread::hardware_concurrency();
+
+    scores[threadId * hwThreads + otherThreadId] = 1 / speed * 1000000000;
+}
+
+void performc2c()
+{
+    int hwThreads = std::thread::hardware_concurrency();
+
+    std::cout << "Hardware threads: " << hwThreads << std::endl;
+
+    size_t* testval = new size_t[1];
+    double *scores = new double[hwThreads * hwThreads];
+
+    for (size_t i = 0; i < hwThreads; i++)
+    {
+        for (size_t j = 0; j < hwThreads; j++)
+        {
+            if (i == j)
+            {
+                scores[i * hwThreads + j] = 0;
+            }
+
+            testval[0] = j;
+            size_t* start = new size_t[1];
+
+            testatomic.store(j);
+
+            std::thread threadA(c2ctest, i, j, start, scores);
+            std::thread threadB(c2ctest, j, i, start, nullptr);
+
+            SetThreadAffinityMask(threadA.native_handle(), 1 << i);
+            SetThreadAffinityMask(threadB.native_handle(), 1 << j);
+
+            // theads are spinning, start the test
+            start[0] = 0x1234;
+
+            threadA.join();
+            threadB.join();
+
+            std::cout << "thread " << i << " to " << j << " latency: " << scores[i * hwThreads + j] << " ns\n";
+
+            delete[] start;
+        }
+	}
+
+    for (size_t i = 0; i < hwThreads; i++)
+    {
+        for (size_t j = 0; j < hwThreads; j++)
+        {
+			std::cout << scores[i * hwThreads + j] << ", ";
+		}
+		std::cout << ";\n";
+    }
+
+    delete[] scores;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    size_t threads = 1;
+
+    //performc2c();
+
+    //return 0;
+    for (size_t s = 1; s < 24; s++)
+    {
+        size_t count = pow(2,s) * 128 / sizeof(size_t); // 128 MB
+
+        std::thread* thread = new std::thread[threads];
+
+        double* scores = new double[threads];
+
+        size_t* start = new size_t[1];
+
+        for (size_t i = 0; i < threads; i++)
+        {
+            thread[i] = std::thread(memRandAccess, count, i,start, scores);
+            SetThreadAffinityMask(thread[i].native_handle(), 1 << i);
+        }
+
+        start[0] = 0x1234;
+
+        for (size_t i = 0; i < threads; i++)
+        {
+            thread[i].join();
+        }
+
+        double speed = 0;
+
+        for (size_t i = 0; i < threads; i++)
+        {
+            speed += scores[i];
+        }
+
+        std::cout << "work size: " << count * sizeof(size_t) / 1024 << " KB, speed: " << speed << " ns\n";
+    }
+}
